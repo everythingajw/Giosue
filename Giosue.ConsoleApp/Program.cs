04af572c712a865280b772849867ef7d48b6e997
Anthony Webster <ajwebster1@gmail.com>
2021-02-18 15:06:58 -0500

    Add project files.

---
+++ Giosue.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Giosue.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
