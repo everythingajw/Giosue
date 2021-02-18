ddc5d9719fcc599a779ebbe894bfd6d8b193979c
Anthony Webster <ajwebster1@gmail.com>
2021-02-18 17:10:30 -0500

    Add basic fields and a constructor for the Scanner class.

--- Giosue/Scanner.cs
+++ Giosue/Scanner.cs
@@ -6,5 +6,12 @@ namespace Giosue
 {
     class Scanner
     {
+        private string Source { get; }
+        private List<Token> Tokens { get; } = new();
+
+        public Scanner(string source)
+        {
+            Source = source ?? throw new ArgumentNullException(nameof(source), "The source for a scanner cannot be null.");
+        }
     }
 }
