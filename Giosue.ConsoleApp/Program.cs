e6f4ea8aad03a3087b7f9f28059d921342a947a3
Anthony Webster <ajwebster1@gmail.com>
2021-02-20 02:07:52 -0500

    Add comments.

--- Giosue.ConsoleApp/Program.cs
+++ Giosue.ConsoleApp/Program.cs
@@ -19,59 +19,46 @@ namespace Giosue.ConsoleApp
             var testCode = File.ReadAllText(TestCodePath);
             var scanner = new Scanner(testCode);
 
+            // This really isn't the most elegant way to handle these errors,
+            // but it's easy and rather simple to implement.
             try
             {
+                // Try scanning
                 var tokens = scanner.ScanTokens();
+                
+                // Print tokens if successful
                 PrettyPrintTokens(tokens);
                 Console.WriteLine();
                 Console.WriteLine("Scanning successful.");
             }
             catch (UnexpectedCharacterException e)
             {
+                // Show any unexpected characters
                 PrettyPrintTokens(scanner.GetTokens());
                 Console.WriteLine();
                 Console.WriteLine(e.Message);
-                Console.WriteLine($"Character: {e.UnexpectedCharacter}");
+                Console.WriteLine($"Character: '{e.UnexpectedCharacter}'");
                 Console.WriteLine($"Line: {e.Line}");
             }
             catch (Exception e)
             {
+                // Show any other errors
                 PrettyPrintTokens(scanner.GetTokens());
                 Console.WriteLine();
                 Console.WriteLine("An error occurred");
                 Console.WriteLine(e.Message);
             }
 
-            Console.WriteLine();
+            Console.WriteLine("\nPress any key to continue...");
             Console.ReadKey();
-        }
-
-        private static IEnumerable<IEnumerable<Token>> GroupTokens(IEnumerable<Token> tokens)
-        {
-            return tokens.GroupBy(t => t.Line);
-        }
-
-        private static void PrintTokens(IEnumerable<Token> tokens)
-        {
-            PrintTokens(GroupTokens(tokens));
-        }
-
-        private static void PrintTokens(IEnumerable<IEnumerable<Token>> tokens)
-        {
-            foreach (var group in tokens)
-            {
-                foreach (var token in group)
-                {
-                    Console.WriteLine(token.ToString());
-                }
-                Console.WriteLine();
-            }
         }
 
         private static void PrettyPrintTokens(IEnumerable<Token> tokens)
         {
+            // Stringify the tokens
             var stringifiedTokens = tokens.Select(t => (t.Type.ToString(), t?.Lexeme?.ToString() ?? "", t?.Literal?.ToString() ?? "", t.Line.ToString()));
 
+            // Calculate the length for each column
             var columnLengths = new int[]
             {
                 stringifiedTokens.Max(t => t.Item1.Length),
@@ -80,6 +67,7 @@ namespace Giosue.ConsoleApp
                 stringifiedTokens.Max(t => t.Item4.Length),
             };
 
+            // Get the column headers
             var headers = new string[]
             {
                 $"{nameof(Token.Type).PadRight(columnLengths[0])} | ",
@@ -88,13 +76,17 @@ namespace Giosue.ConsoleApp
                 $"{nameof(Token.Line).PadRight(columnLengths[3])}"
             };
 
+            // Write the headers
             foreach (var header in headers)
             {
                 Console.Write(header);
             }
             Console.WriteLine();
+
+            // Make a line between the headers and the data
             Console.WriteLine("".PadLeft(headers.Sum(h => h.Length), '-'));
 
+            // Print the tokens
             foreach (var token in stringifiedTokens)
             {
                 Console.Write($"{token.Item1.PadRight(columnLengths[0])} | ");
