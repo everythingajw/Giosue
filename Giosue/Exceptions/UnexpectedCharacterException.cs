119ee67db5d94c3e0de9c3593331bac4637abb40
Anthony Webster <ajwebster1@gmail.com>
2021-02-20 01:44:31 -0500

    Add UnexpectedCharacterException.

---
+++ Giosue/Exceptions/UnexpectedCharacterException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giosue.Exceptions
+{
+    public class UnexpectedCharacterException : Exception
+    {
+        public int Line { get; } = 0;
+
+        public UnexpectedCharacterException() : base()
+        {
+
+        }
+
+        public UnexpectedCharacterException(string message) : base(message)
+        {
+
+        }
+
+        public UnexpectedCharacterException(int line, string message) : this(message)
+        {
+            Line = line;
+        }
+
+        public UnexpectedCharacterException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+
+        public UnexpectedCharacterException(int line, string message, Exception innerException) : this(message, innerException)
+        {
+            Line = line;
+        }
+    }
+}
