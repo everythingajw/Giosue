a2c00fb170d567388178ede70d0d21da55296806
Anthony Webster <ajwebster1@gmail.com>
2021-02-22 10:57:37 -0500

    Implement constructor and add some documentation for inherited members.

--- SourceManager/StringSource.cs
+++ SourceManager/StringSource.cs
@@ -8,35 +8,54 @@ namespace SourceManager
 {
     public class StringSource : ISource
     {
+        private string Source { get; }
+
+        private int TokenStartIndex { get; }
+
+        private int CurrentCharacterIndex { get; }
+
+        /// <inheritdoc/>
         public bool IsAtEnd => throw new NotImplementedException();
 
+        /// <inheritdoc/>
         public string CurrentToken => throw new NotImplementedException();
 
+        public StringSource(string source)
+        {
+            Source = source ?? throw new ArgumentNullException(nameof(source), $"The source for a {nameof(StringSource)} cannot be null.");
+        }
+
+        /// <inheritdoc/>
         public bool Advance(out char consumed)
         {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc/>
         public bool AdvanceIfMatches(char c, out char consumed)
         {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc/>
         public void ClearToken()
         {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc/>
         public void Dispose()
         {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc/>
         public bool Peek(out char current)
         {
             throw new NotImplementedException();
         }
 
+        /// <inheritdoc/>
         public bool PeekNext(out char next)
         {
             throw new NotImplementedException();
