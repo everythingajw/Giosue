1f671c7a55daa1d7264d7912e265ba7afb10f153
Anthony Webster <ajwebster1@gmail.com>
2021-03-02 12:10:19 -0500

    Return false when Peek fails for AdvanceIfMatches in FileSource.

--- SourceManager/FileSource.cs
+++ SourceManager/FileSource.cs
@@ -281,6 +281,7 @@ namespace SourceManager
                 {
                     return false;
                 }
+                return false;
             }
 
             // This is safe because we already checked that we're not at the end
