0ab969b2e472fe92eafc6f997993c0ebeb0600e0
Anthony Webster <ajwebster1@gmail.com>
2021-04-03 19:48:54 -0400

    Outline for AST printer.

---
+++ Giosue/ASTPrinter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Giosue.AST;
+
+namespace Giosue
+{
+    class ASTPrinter : IVisitor<string>
+    {
+        public string VisitAssignExpression(Assign expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string VisitBinaryExpression(Binary expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string VisitCallExpression(Call expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string VisitGetExpression(Get expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string VisitGroupingExpression(Grouping expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string VisitLiteralExpression(Literal expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string VisitLogicalExpression(Logical expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string VisitSetExpression(Set expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string VisitSuperExpression(Super expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string VisitThisExpression(This expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string VisitUnaryExpression(Unary expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string VisitVariableExpression(Variable expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
