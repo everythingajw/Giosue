b58d959c295b0ca477f8390dd0cde877ffb1e198
Anthony Webster <ajwebster1@gmail.com>
2021-04-03 18:20:43 -0400

    Add a class for the parser.

---
+++ Giosue/Parser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giosue
+{
+    public class Parser
+    {
+        private List<Token> Tokens { get; }
+        private int CurrentTokenIndex = 0;
+
+        public Parser(List<Token> tokens)
+        {
+            Tokens = tokens ?? throw new ArgumentNullException(nameof(tokens), $"The source tokens for a {nameof(Parser)} cannot be null");
+        }
+    }
+}
