cb48b9d89fd46107731c634d036170caab710292
Anthony Webster <ajwebster1@gmail.com>
2021-04-19 17:16:50 -0400

    Add EnvironmentException.

---
+++ Giosue/Exceptions/EnvironmentException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giosue.Exceptions
+{
+    public enum EnvironmentExceptionType
+    {
+        AllOK = 0,
+
+    }
+    
+    public class EnvironmentException : GiosueException<EnvironmentExceptionType>
+    {
+        public override EnvironmentExceptionType ExceptionType { get; }
+
+        public EnvironmentException() : base(default) { }
+        public EnvironmentException(string message) : base(message) { }
+        public EnvironmentException(EnvironmentExceptionType environmentExceptionType, string message = default) : this(message)
+        {
+            ExceptionType = environmentExceptionType;
+        }
+    }
+}
